4909eef63eb6198a08fac7aecd159967a200e391
lyh990517 <lyh990517@naver.com>
2024-12-03 15:14:43 +0900

    add custom decoration

--- app/src/main/java/com/yunho/circuitsample/MainActivity.kt
+++ app/src/main/java/com/yunho/circuitsample/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.slack.circuit.backstack.NavDecoration
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
+import com.slack.circuit.foundation.NavigatorDefaults
+import com.slack.circuit.foundation.NavigatorDefaults.DefaultDecoration.backward
+import com.slack.circuit.foundation.NavigatorDefaults.DefaultDecoration.forward
 import com.slack.circuit.foundation.rememberCircuitNavigator
 import com.yunho.circuitsample.ui.theme.CircuitSampleTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.collections.immutable.ImmutableList
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -30,11 +42,31 @@ class MainActivity : ComponentActivity() {
 
                 CircuitCompositionLocals(circuit) {
                     NavigableCircuitContent(
+                        decoration = CustomDecoration,
                         navigator = navigator,
                         backStack = backStack
                     )
                 }
             }
+        }
+    }
+}
+
+object CustomDecoration : NavDecoration {
+    @Composable
+    override fun <T> DecoratedContent(
+        args: ImmutableList<T>,
+        backStackDepth: Int,
+        modifier: Modifier,
+        content: @Composable (T) -> Unit
+    ) {
+        AnimatedContent(
+            targetState = args,
+            modifier = modifier,
+            transitionSpec = { fadeIn() togetherWith fadeOut() },
+            label = "",
+        ) { arguments ->
+            content(arguments.first())
         }
     }
 }
